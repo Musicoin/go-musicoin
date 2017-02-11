7160d27af8b90564021bc53d8e9009355100b9ea
isaac <isaac.mao@gmail.com>
2017-02-11 11:36:36 +0800

    parity test

--- core/fees.go
+++ core/fees.go
@@ -20,4 +20,4 @@ import (
 	"math/big"
 )
 
-var BlockReward *big.Int = new(big.Int).Mul(big.NewInt(314), big.NewInt(1e+18))
+var BlockReward *big.Int = new(big.Int).Mul(big.NewInt(500), big.NewInt(1e+18))
