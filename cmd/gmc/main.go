e7ce8aba22931dfe64a441e8cce66d548e54f113
im <im@berry.ai>
2017-09-08 20:21:24 +0800

    Final versioning

--- cmd/gmc/main.go
+++ cmd/gmc/main.go
@@ -48,8 +48,8 @@ import (
 
 const (
 	clientIdentifier = "GMC"   // Client identifier to advertise over the network
-	versionMajor     = 1        // Major version component of the current release
-	versionMinor     = 5        // Minor version component of the current release
+	versionMajor     = 2        // Major version component of the current release
+	versionMinor     = 0        // Minor version component of the current release
 	versionPatch     = 0       // Patch version component of the current release
 	versionMeta      = "stable" // Version metadata to append to the version string
 
