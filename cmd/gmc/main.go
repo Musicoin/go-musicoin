28cc115ff540dc50bea2d6e23e0098f0f2f752da
5chdn <5chdn@users.noreply.github.com>
2017-10-22 09:26:23 +0200

    Add license to help, #60

--- cmd/gmc/main.go
+++ cmd/gmc/main.go
@@ -140,7 +140,21 @@ func init() {
 	// Initialize the CLI app and start GMC
 	app.Action = gmc
 	app.HideVersion = true // we have a command to print the version
-	app.Copyright = "Copyright 2013-2017 The go-ethereum Authors"
+	app.Copyright = `Copyright 2013-2017 The Go-Ethereum Authors
+   Copyright 2016-2017 The Go-Musicoin Authors
+
+   Go-Musicoin is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   Go-Musicoin is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with Go-Musicoin. If not, see <http://www.gnu.org/licenses/>.`
 	app.Commands = []cli.Command{
 		// See chaincmd.go:
 		initCommand,
