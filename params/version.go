039a2d998cb789dd49317807953b2f56bf7f9b94
isaac <isaac.mao@gmail.com>
2017-11-08 14:12:41 +0800

    fully tested to be stable

--- params/version.go
+++ params/version.go
@@ -23,8 +23,8 @@ import (
 const (
 	VersionMajor = 2          // Major version component of the current release
 	VersionMinor = 5          // Minor version component of the current release
-	VersionPatch = 0          // Patch version component of the current release
-	VersionMeta  = "unstable" // Version metadata to append to the version string
+	VersionPatch = 1          // Patch version component of the current release
+	VersionMeta  = "stable" // Version metadata to append to the version string
 )
 
 // Version holds the textual version string.
