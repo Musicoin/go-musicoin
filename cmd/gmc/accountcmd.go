989a99902c1f0681836faecca494b294cbb14e03
5chdn <5chdn@users.noreply.github.com>
2017-11-27 12:30:31 +0100

    Use gmc as account command

--- cmd/gmc/accountcmd.go
+++ cmd/gmc/accountcmd.go
@@ -291,7 +291,7 @@ func ambiguousAddrRecovery(ks *keystore.KeyStore, err *keystore.AmbiguousAddrErr
 
 // accountCreate creates a new account into the keystore defined by the CLI flags.
 func accountCreate(ctx *cli.Context) error {
-	cfg := gethConfig{Node: defaultNodeConfig()}
+	cfg := gmcConfig{Node: defaultNodeConfig()}
 	// Load config file.
 	if file := ctx.GlobalString(configFileFlag.Name); file != "" {
 		if err := loadConfig(file, &cfg); err != nil {
