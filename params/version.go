b4e0f8d029753b1dd70824f8be2e20bf037752d4
im <isaac@musicoin.org>
2019-01-03 10:59:23 +0800

    versioning

--- params/version.go
+++ params/version.go
@@ -22,8 +22,8 @@ import (
 
 const (
 	VersionMajor = 2          // Major version component of the current release
-	VersionMinor = 6          // Minor version component of the current release
-	VersionPatch = 2          // Patch version component of the current release
+	VersionMinor = 7          // Minor version component of the current release
+	VersionPatch = 0          // Patch version component of the current release
 	VersionMeta  = "beta" // Version metadata to append to the version string
 )
 
