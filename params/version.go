7315081e469286b3c4c2449e314289df79d2fb42
im <isaac@musicoin.org>
2019-01-05 16:38:16 +0800

    final check

--- params/version.go
+++ params/version.go
@@ -23,8 +23,8 @@ import (
 const (
 	VersionMajor = 2          // Major version component of the current release
 	VersionMinor = 7          // Minor version component of the current release
-	VersionPatch = 0          // Patch version component of the current release
-	VersionMeta  = "beta" // Version metadata to append to the version string
+	VersionPatch = 1          // Patch version component of the current release
+	VersionMeta  = "final" // Version metadata to append to the version string
 )
 
 // Version holds the textual version string.
