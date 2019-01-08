462506ec533ea43a0a687777432cd5288627bf5e
Im <isaac.mao@gmail.com>
2019-01-08 08:55:28 +0800

    Revert "Pirlguard installation"

--- core/error.go
+++ core/error.go
@@ -32,7 +32,4 @@ var (
 	// ErrNonceTooHigh is returned if the nonce of a transaction is higher than the
 	// next one expected based on the local chain.
 	ErrNonceTooHigh = errors.New("nonce too high")
-	ErrDelayTooHigh = errors.New("Chain time values are not right!")
-
-
 )
