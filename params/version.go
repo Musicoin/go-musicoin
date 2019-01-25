280f76ddd51d78981ac4afe833733e9164b240e8
im <isaac@musicoin.org>
2019-01-25 03:41:19 +0000

    revert to v2.8.1 since 2.9 soluiton harbored hardfork

--- params/version.go
+++ params/version.go
@@ -22,8 +22,8 @@ import (
 
 const (
 	VersionMajor = 2          // Major version component of the current release
-	VersionMinor = 9          // Minor version component of the current release
-	VersionPatch = 0          // Patch version component of the current release
+	VersionMinor = 8          // Minor version component of the current release
+	VersionPatch = 1          // Patch version component of the current release
 	VersionMeta  = "final" // Version metadata to append to the version string
 )
 
